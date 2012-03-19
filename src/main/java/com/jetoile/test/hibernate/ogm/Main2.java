9a5ee1b561fcdbcfac829fb8a0446949702045a2
jetoile <kmx.petals@gmail.com>
2012-03-19 08:53:04 +0100

    licence + gitignore

--- src/main/java/com/jetoile/test/hibernate/ogm/Main2.java
+++ src/main/java/com/jetoile/test/hibernate/ogm/Main2.java
@@ -1,22 +1,25 @@
 /*
- * Hibernate, Relational Persistence for Idiomatic Java
+ * Copyright (c) 2011 Khanh Tuong Maudoux <kmx.petals@gmail.com>
+ * All rights reserved.
  *
- * JBoss, Home of Professional Open Source
- * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the @authors tag. All rights reserved.
- * See the copyright.txt in the distribution for a
- * full listing of individual contributors.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
  *
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU Lesser General Public License, v. 2.1.
- * This program is distributed in the hope that it will be useful, but WITHOUT A
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
- * You should have received a copy of the GNU Lesser General Public License,
- * v.2.1 along with this distribution; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package com.jetoile.test.hibernate.ogm;
 
@@ -32,7 +35,6 @@ import javax.transaction.TransactionManager;
 
 import org.infinispan.transaction.lookup.JBossStandaloneJTAManagerLookup;
 
-import org.hibernate.transaction.JBossTSStandaloneTransactionManagerLookup;
 
 /**
  * User: khanh
@@ -40,13 +42,12 @@ import org.hibernate.transaction.JBossTSStandaloneTransactionManagerLookup;
  * Time: 17:29
  */
 public class Main2 {
-	
+
 	public static void main(String[] args)
 			throws Exception, RollbackException, HeuristicRollbackException, HeuristicMixedException, NotSupportedException {
 
-//		TransactionManager tm = new JBossStandaloneJTAManagerLookup().getTransactionManager();
-		TransactionManager tm = new JBossTSStandaloneTransactionManagerLookup().getTransactionManager(null);
-
+		TransactionManager tm = new JBossStandaloneJTAManagerLookup().getTransactionManager();
+//		TransactionManager tm = new JBossTSStandaloneTransactionManagerLookup().getTransactionManager(null);
 
 
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory(
@@ -58,7 +59,7 @@ public class Main2 {
 		EntityManager em = emf.createEntityManager();
 		Breed norvegien = new Breed();
 		norvegien.setName( "norvegien" );
-		norvegien.setId("1");
+		norvegien.setId( "1" );
 		em.persist( norvegien );
 
 		em.flush();
